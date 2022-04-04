76594d728bb4ed19cabd0be74eede29868fa9312
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-04-04 13:20:07 +0530

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystewm
+{
+    class Address
+    {
+        public string AddressBookName;
+        public string FirstName;
+        public string LastName;
+        public string address;
+        public string Email;
+        public string PhoneNumber;
+        public string State;
+        public string Zip;
+        public string Country;
+
+        public Address(string addressbookname , string firstname, string lastname, string address, string email, string phonenumber, string state, string zip, string country)
+        {
+            this.AddressBookName = addressbookname;
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.address = address;
+            this.Email = email;
+            this.PhoneNumber = phonenumber;
+            this.State = state;
+            this.Zip = zip; 
+            this.Country = country;
+        }
+    }
+}
---
+++ AddressBook.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystewm
+{
+    class AddressBook
+    {
+        List<Address> addresses;
+
+        public AddressBook()
+        {
+            addresses = new List<Address>();
+        }
+
+        public bool add(string addressbookname, string firstname, string lastname, string address, string email, string phonenumber, string state, string zip, string country)
+        {
+            Address addr = new Address(addressbookname,firstname, address, lastname, email, phonenumber, state, zip, country);
+            Address result = find(firstname);
+
+            if (result == null)
+            {
+                addresses.Add(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool remove(string firstname)
+        {
+            Address addr = find(firstname);
+
+            if (addr != null)
+            {
+                addresses.Remove(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void list(Action<Address> action)
+        {
+            addresses.ForEach(action);
+        }
+
+        public bool isEmpty()
+        {
+            return (addresses.Count == 0);
+        }
+
+        public Address find(string firstname)
+        {
+            Address addr = addresses.Find((a) => a.FirstName == firstname);
+            return addr;
+        }
+    }
+}
